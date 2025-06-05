103ccc3d3ad6b61614725a032f72f2bdd4583741
Viktor <brnovk@gmail.com>
2025-06-05 19:28:24 +0300

    Update With s3.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 const axios = require("axios");
 const FormData = require("form-data");
+
 const fs = require("fs");
+const fsp = fs.promises;
+
+const {
+	GetObjectCommand,
+	S3Client
+} = require("@aws-sdk/client-s3");
+const s3Client = new S3Client({});
 
 let currentContext;
 
@@ -10,44 +18,54 @@ exports.handler = async (event, context, callback) => {
 	try {
 		let url = "https://test12345.my.salesforce.com/services/data/v61.0/sobjects/ContentVersion";
 		let method = "POST";
-		const fs = require("fs");
-		const fsp = fs.promises;
+
+		// local
 		let file = await fsp.readFile("files/" + "Test (1).pdf");
-		let base64File = file.toString("base64");
+		let base64VersionData = file.toString("base64");
+
+		// // S3
+		// // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/s3/command/GetObjectCommand/
+		// let s3Bucket = "vik-test-bucket-3";
+		// let s3Filename = "Test (1).pdf";
+		// let s3ContentType = null;
+		// let s3params = {
+		// 	"Bucket" : s3Bucket,
+		// 	"Key" : s3Filename
+		// };
+		// if (s3ContentType) {
+		// 	s3params["ResponseContentType"] = s3ContentType;
+		// }
+		// let s3Response = await s3Client.send(new GetObjectCommand(s3params));
+		// // "transformToString", "transformToByteArray" and "transformToWebStream"
+		// let base64VersionData = await s3Response.Body.transformToString("base64");
+
 		let data = [
 			{
 				"name": "entity_content",
-				"value": {
+				"value": JSON.stringify({
 					"ContentDocumentId": "069KB000000TuyaYAC",
 					"PathOnClient": "Test (1).pdf",
-					"ReasonForChange": "update124"
-				},
-				"convertValue": "stringify",
+					"ReasonForChange": "update125"
+				}),
 				"formAppendOptions": {
 					"contentType": "application/json"
 				}
 			},
 			{
 				"name": "VersionData",
-				"value": base64File,
-				"convertValue": "base64ToBlob",
+				"value": Buffer.from(base64VersionData, "base64"),
 				"formAppendOptions": {
 					"filename": "Test (1).pdf",
 					"contentType": "application/pdf"
 				}
 			}
 		];
+
 		let form = new FormData();
 		for (let multipartFormDataParam of data) {
 			let name = multipartFormDataParam.name;
 			let value = multipartFormDataParam.value;
-			let convertValue = multipartFormDataParam.convertValue;
 			let formAppendOptions = multipartFormDataParam.formAppendOptions;
-			if (isString(convertValue) && convertValue.toLowerCase() === "stringify".toLowerCase()) {
-				value = JSON.stringify(value);
-			} else if (isString(convertValue) && convertValue.toLowerCase() === "base64ToBlob".toLowerCase()) {
-				value = Buffer.from(value, "base64");
-			}
 			if (formAppendOptions) {
 				form.append(name, value, formAppendOptions);
 			} else {
@@ -55,9 +73,8 @@ exports.handler = async (event, context, callback) => {
 			}
 		}
 		let headers = form.getHeaders(); // Specify "Content-Type": multipart/form-data; boundary=...
-		console.log(headers);
 
-		headers["Authorization"] = "Bearer 00D2X000002ES8q!......";
+		headers["Authorization"] = "Bearer 00D2X000002ES8q!.....";
 
 		let params = {};
 		params.url = url;
@@ -97,7 +114,6 @@ let process = {
 
 // RUNNER
 exports.handler({
-
 }).then(function(data) {
 	console.log(JSON.stringify(data, null,2));
 	console.log("------------------------------------------------------------------------");
